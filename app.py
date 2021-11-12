3104922aa26b72a2de1959e64529f643d4bc8859
Hank <hank.likes.matcha@gmail.com>
2021-11-13 00:37:21 +1300

    simplify the process

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from __future__ import print_function
-import pickle
-import os.path
-import sys
 import argparse
+import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 import base64
 import re
 from datetime import date, timedelta, datetime
-from openpyxl import Workbook, load_workbook
+from openpyxl import Workbook
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
@@ -19,15 +18,28 @@ file_name = "gift-codes"
 ws.title = "Gift Codes"
 
 
+def generate_file(count: int, codes: []):
+    print("estimated:{}".format(count))
+    while count != 0:
+        if count != len(codes):
+            print("Number doesn't match with the total orders")
+            return
+        for index, code in enumerate(codes):
+            ws.append([code])
+            count -= 1
+        wb.save(file_name + "{}.xlsx".format(date.today()))
+        print("total orders {}".format(len(codes)))
+    return count
+
+
 def main(sender: str, starts_from: str):
     # 'hello@thegoodregistry.com'
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
+    # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
-            creds = pickle.load(token)
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -36,8 +48,8 @@ def main(sender: str, starts_from: str):
             flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("token.pickle", "wb") as token:
-            pickle.dump(creds, token)
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
 
     service = build("gmail", "v1", credentials=creds)
 
@@ -54,64 +66,37 @@ def main(sender: str, starts_from: str):
     )
     count = 0
 
-    gift_codes = []
-
     if len(mail_group["messages"]) < 1:
         print("No sender found")
 
-    next_page = True
-
-    def generate_file(count: int, codes: []):
-        while count != 0:
-            if count != len(codes):
-                print("Number doesn't match with the total orders")
-                return
-            for index, code in enumerate(gift_codes):
-                ws.append([code])
-                count -= 1
-            file = wb.save(file_name + "{}.xlsx".format(date.today()))
-
-            print("total orders {}".format(len(codes)))
-        return count
-
-    while next_page:
-        if next_page is False:
-            break
-
-        for index, email in enumerate(mail_group["messages"]):
+    gift_codes: list = []
+    if not mail_group.get("nextPageToken"):
+        mail_group = (
+            service.users()
+            .messages()
+            .list(
+                userId="me",
+                q="from='{}' after: {}".format(sender, timestamp),
+                pageToken=mail_group.get("nextPageToken"),
+            )
+            .execute()
+        )["messages"]
+        for index, email in enumerate(mail_group):
             raw_contents = (
                 service.users().messages().get(userId="me", id=email["id"]).execute()
-            )
-            encoded_contents = raw_contents["payload"]["parts"][0]["body"]["data"]
-            decoded_contents = base64.urlsafe_b64decode(encoded_contents).decode(
-                "utf-8"
-            )
+            )["payload"]["parts"][0]["body"]["data"]
+            decoded_contents = base64.urlsafe_b64decode(raw_contents).decode("utf-8")
             match = re.search("and your Gift Card Code is\s+", decoded_contents)
 
             if match is None:
-                return
+                generate_file(count=count, codes=gift_codes)
 
-            matched_number = decoded_contents[match.end() :]
-            if len(matched_number) != 16:
-                print("no gift card code found")
-            count += 1
-            gift_codes.append(matched_number)
-
-        if mail_group.get('nextPageToken'):
-            mail_group = (
-                service.users()
-                .messages()
-                .list(
-                    userId="me",
-                    q="from='{}' after: {}".format(sender, timestamp),
-                    pageToken=mail_group["nextPageToken"],
-                )
-                .execute()
-            )
-        else:
-            next_page = False 
-            print("estimated:{}".format(count))
-            generate_file(count=count, codes=gift_codes)
+            else:
+                matched_number = decoded_contents[match.end() :]
+                if len(matched_number) != 16:
+                    print("no gift card code found")
+                count += 1
+                gift_codes.append(matched_number)
 
 
 parser = argparse.ArgumentParser(
